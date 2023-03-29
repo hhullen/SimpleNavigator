1d207abf2719996ed7b8bb1f57915e7b6a5b4002
Ivelisse Sigrin <isigrin@in-f2.nsk.21-school.ru>
2023-03-29 18:44:40 +0700

    update: add algorithms - DFS, BFS, WFI,Dijkstra’s

--- src/algorithms/graph_algorithms.cc
+++ src/algorithms/graph_algorithms.cc
@@ -3,5 +3,99 @@
 namespace s21 {
 
 GraphAlgorithms::GraphAlgorithms() {}
+vector<int> GraphAlgorithms::depthFirstSearch(Graph &graph, int startVertex) {
+  vector<int> result;
+  vector<bool> visited;
+  stack<int> stack_vertex;
+  int pop_vertex = 0;
+  int matrix_size = graph.get_size();
+  visited.resize(matrix_size);
+  stack_vertex.push(startVertex - 1);
+  while (!stack_vertex.empty()) {
+    pop_vertex = stack_vertex.top();
+    stack_vertex.pop();
+    if (!visited[pop_vertex]) {
+      visited[pop_vertex] = 1;
+      for (int i = matrix_size - 1; i >= 0; --i) {
+        if (graph(pop_vertex, i) && !visited[i]) {
+          stack_vertex.push(i);
+        }
+      }
+      result.push_back(pop_vertex + 1);
+    }
+  }
+  return result;
+}
 
-}  // namespace s21
+vector<int> GraphAlgorithms::breadthFirstSearch(Graph &graph, int startVertex) {
+  vector<int> result;
+  vector<bool> visited;
+  queue<int> queue_vertex;
+  int matrix_size = graph.get_size();
+  visited.resize(matrix_size);
+  queue_vertex.push(startVertex - 1);
+  int pop_vertex = 0;
+  while (!queue_vertex.empty()) {
+    pop_vertex = queue_vertex.front();
+    queue_vertex.pop();
+    if (!visited[pop_vertex]) {
+      visited[pop_vertex] = 1;
+      for (int i = 0; i < matrix_size; ++i) {
+        if (graph(pop_vertex, i) && !visited[i]) {
+          queue_vertex.push(i);
+        }
+      }
+      result.push_back(pop_vertex + 1);
+    }
+  }
+  return result;
+}
+
+int GraphAlgorithms::getShortestPathBetweenVertices(Graph &graph, int vertex1,
+                                                    int vertex2) {
+  vector<bool> visited;
+  queue<int> queue_vertex;
+  int matrix_size = graph.get_size();
+  vector<int> weights(matrix_size, INT_MAX);
+  weights[vertex1 - 1] = 0;
+  visited.resize(matrix_size);
+  queue_vertex.push(vertex1 - 1);
+  int pop_vertex = 0;
+  while (!queue_vertex.empty()) {
+    pop_vertex = queue_vertex.front();
+    queue_vertex.pop();
+    if (!visited[pop_vertex]) {
+      visited[pop_vertex] = 1;
+      for (int i = 0; i < matrix_size; ++i) {
+        if (graph(pop_vertex, i)) {
+          if (weights[i] > weights[pop_vertex] + graph(pop_vertex, i)) {
+            weights[i] = weights[pop_vertex] + graph(pop_vertex, i);
+          }
+          if (!visited[i]) {
+            queue_vertex.push(i);
+          }
+        }
+      }
+    }
+  }
+  return weights[vertex2 - 1];
+}
+
+Graph GraphAlgorithms::getShortestPathsBetweenAllVertices(Graph &graph) {
+  int graph_size = graph.get_size();
+  Graph result(graph_size);
+  result = graph;
+  for (int k = 0; k < graph_size; ++k) {
+    for (int i = 0; i < graph_size; ++i) {
+      for (int j = 0; j < graph_size; ++j) {
+        if (result(i, k) && result(k, j) && i != j) {
+          if (result(i, j) > result(i, k) + result(k, j) || result(i, j) == 0) {
+            result(i, j) = result(i, k) + result(k, j);
+          }
+        }
+      }
+    }
+  }
+  return result;
+}
+} // namespace s21
